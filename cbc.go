e6ed33a0a5f1322ecf907db24a0631d4d20bc007
junjuncai <15229895912@163.com>
2022-12-15 10:01:25 +0800

    1.modify cbc cipher.

--- cbc.go
+++ cbc.go
@@ -16,9 +16,9 @@ package crypto
 import "crypto/cipher"
 
 type CBCCipher struct {
-	block   cipher.Block
-	iv      []byte
 	padding PaddingT
+	enBlock cipher.BlockMode
+	deBlock cipher.BlockMode
 }
 
 // *********************************************************************************************************************
@@ -26,12 +26,14 @@ type CBCCipher struct {
 // * WARNING:
 // * HISTORY:
 // *    -create: 2022/11/07 10:45:43 ColeCai.
+// *	-update: 2022/12/15 09:51:03 ColeCai.
+// *			 init encrypt and decrypt block in CBCCipher construction stage.
 // *********************************************************************************************************************
 func NewCBCCipher(block cipher.Block, iv []byte, padding PaddingT) *CBCCipher {
 	return &CBCCipher{
-		block:   block,
-		iv:      iv,
 		padding: padding,
+		enBlock: cipher.NewCBCEncrypter(block, iv),
+		deBlock: cipher.NewCBCDecrypter(block, iv),
 	}
 }
 
@@ -40,12 +42,12 @@ func NewCBCCipher(block cipher.Block, iv []byte, padding PaddingT) *CBCCipher {
 // * WARNING:
 // * HISTORY:
 // *    -create: 2022/11/07 10:47:24 ColeCai.
+// *	-update: 2022/12/15 09:54:08 ColeCai.
 // *********************************************************************************************************************
 func (c *CBCCipher) Encrypt(src []byte) ([]byte, error) {
-	padding := Padding(c.padding, src, c.block.BlockSize())
+	padding := Padding(c.padding, src, c.enBlock.BlockSize())
 	encrypted := make([]byte, len(padding))
-	cbc := cipher.NewCBCEncrypter(c.block, c.iv)
-	cbc.CryptBlocks(encrypted, padding)
+	c.enBlock.CryptBlocks(encrypted, padding)
 	return encrypted, nil
 }
 
@@ -54,10 +56,10 @@ func (c *CBCCipher) Encrypt(src []byte) ([]byte, error) {
 // * WARNING:
 // * HISTORY:
 // *    -create: 2022/11/07 10:50:10 ColeCai.
+// *	-update: 2022/12/15 10:00:03 ColeCai.
 // *********************************************************************************************************************
 func (c *CBCCipher) Decrypt(src []byte) ([]byte, error) {
 	decrypted := make([]byte, len(src))
-	cbc := cipher.NewCBCDecrypter(c.block, c.iv)
-	cbc.CryptBlocks(decrypted, src)
+	c.deBlock.CryptBlocks(decrypted, src)
 	return UnPadding(c.padding, decrypted)
 }
