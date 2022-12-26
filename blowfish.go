ae4fe0a5e4c527b4357d358348984c5066d754ad
junjuncai <15229895912@163.com>
2022-12-26 10:10:21 +0800

    1.add blowfish cfb mode.

--- blowfish.go
+++ blowfish.go
@@ -21,10 +21,24 @@ import "golang.org/x/crypto/blowfish"
 // * HISTORY:
 // *    -create: 2022/12/25 17:22:39 ColeCai.
 // ***********************************************************************************************
-func NewBlowfishCBCCryptor(aesKey, iv []byte, padding padding) (ICryptor, error) {
-	cipher, err := blowfish.NewCipher(aesKey)
+func NewBlowfishCBCCryptor(blowKey, iv []byte, padding padding) (ICryptor, error) {
+	cipher, err := blowfish.NewCipher(blowKey)
 	if err != nil {
 		return nil, err
 	}
 	return NewCBCCipher(cipher, iv, padding), nil
+}
+
+// *********************************************************************************************************************
+// * SUMMARY:
+// * WARNING:
+// * HISTORY:
+// *    -create: 2022/12/26 10:08:40 ColeCai.
+// **************************************************************************************************************
+func NewBlowfishCFBCryptor(blowKey, iv []byte) (ICryptor, error) {
+	cipher, err := blowfish.NewCipher(blowKey)
+	if err != nil {
+		return nil, err
+	}
+	return NewCFBCipher(cipher, iv), nil
 }
