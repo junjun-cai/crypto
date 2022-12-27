68b10d87f51fd476c077c939ffc2ff638a0f2589
junjuncai <15229895912@163.com>
2022-12-27 09:42:35 +0800

    1.add blowfish ecb mode.

--- blowfish.go
+++ blowfish.go
@@ -34,11 +34,25 @@ func NewBlowfishCBCCryptor(blowKey, iv []byte, padding padding) (ICryptor, error
 // * WARNING:
 // * HISTORY:
 // *    -create: 2022/12/26 10:08:40 ColeCai.
-// **************************************************************************************************************
+// *********************************************************************************************************************
 func NewBlowfishCFBCryptor(blowKey, iv []byte) (ICryptor, error) {
 	cipher, err := blowfish.NewCipher(blowKey)
 	if err != nil {
 		return nil, err
 	}
 	return NewCFBCipher(cipher, iv), nil
+}
+
+// *********************************************************************************************************************
+// * SUMMARY:
+// * WARNING:
+// * HISTORY:
+// *    -create: 2022/12/27 09:40:54 ColeCai.
+// *********************************************************************************************************************
+func NewBlowfishECBCryptor(blowKey []byte, padding padding) (ICryptor, error) {
+	cipher, err := blowfish.NewCipher(blowKey)
+	if err != nil {
+		return nil, err
+	}
+	return NewECBCipher(cipher, padding), nil
 }
