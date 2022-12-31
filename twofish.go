43245e2769b890126104dd7c387916691a90fa77
junjuncai <15229895912@163.com>
2022-12-31 23:16:40 +0800

    1.add blowfish cfb mode.

--- twofish.go
+++ twofish.go
@@ -15,16 +15,30 @@ package crypto
 
 import "golang.org/x/crypto/twofish"
 
-// ***********************************************************************************************
+// *********************************************************************************************************************
 // * SUMMARY:
 // * WARNING:
 // * HISTORY:
 // *    -create: 2022/12/30 09:45:34 ColeCai.
-// ***********************************************************************************************
+// *********************************************************************************************************************
 func NewTwofishCBCCryptor(twoKey, iv []byte, padding padding) (ICryptor, error) {
 	cipher, err := twofish.NewCipher(twoKey)
 	if err != nil {
 		return nil, err
 	}
 	return NewCBCCipher(cipher, iv, padding), nil
+}
+
+// *********************************************************************************************************************
+// * SUMMARY:
+// * WARNING:
+// * HISTORY:
+// *    -create: 2022/12/31 23:14:04 ColeCai.
+// *********************************************************************************************************************
+func NewTwofishCFBCryptor(twoKey, iv []byte) (ICryptor, error) {
+	cipher, err := twofish.NewCipher(twoKey)
+	if err != nil {
+		return nil, err
+	}
+	return NewCFBCipher(cipher, iv), nil
 }
